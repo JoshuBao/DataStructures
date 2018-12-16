d1e2b9b8ed1d75ea702ea0d7e758f63303d42221
joshuacheng888 <joshuacheng888@gmail.com>
2018-12-16 14:30:48 -0800

    learing tests unit test

---
+++ Graphs/Graphs/Sorts.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class Sorts
+    {
+        public static void BubbleSort<T>(T[] items) where T : IComparable<T>
+        {
+            bool unsorted = true;
+
+            while (unsorted)
+            {
+                unsorted = false;
+                for (int i = 0; i < items.Length - 1; i++)
+                {
+                    if (items[i].CompareTo(items[i + 1]) > 0)
+                    {
+                        T tmp = items[i];
+                        items[i] = items[i + 1];
+                        items[i + 1] = tmp;
+                        unsorted = true;
+                    }
+                }
+            }
+        }
+
+     
+
+    }
+    class BST<T> where T : IComparable<T>
+    {
+        public TreeNode<T> root;
+        
+        void Add(T value)
+        {
+            if (root == null)
+            {
+                root = new TreeNode<T>(value);
+                return;
+            }
+
+            var curr = root;
+        }
+
+    }
+
+
+
+    class TreeNode<T> where T : IComparable<T>
+    {
+        public TreeNode<T> Left;
+        public TreeNode<T> Right;
+        public T Value;
+        public int ChildCount
+        {
+            get
+            {
+                int count = 0;
+                if (Left != null) count++;
+                if (Right != null) count++;
+                return count;
+            }
+        }
+        public TreeNode(T value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+
+
+}
+
+
+
+
+
---
+++ Graphs/UnitTests/SortTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Graphs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SortTests
+    {
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            int[] items = new int[] { 5, 8, 1, 14, 82, 95, 12 };
+
+            Sorts.BubbleSort(items);
+
+            int[] correctItems = new int[] { 1, 5, 8, 12, 14, 82, 95 };
+
+
+            Assert.AreEqual(items.Length, correctItems.Length);
+
+            for(int i = 0; i < items.Length; i++)
+            {
+                Assert.AreEqual(items[i], correctItems[i]);
+            }
+        }
+        [TestMethod]
+        public void BST()
+        {
+
+
+
+
+        }
+    }
+}
